a79e2abfd2785b9e32588426a10af482cc2a85d3
Gabe Radovsky <gabriel.radovsky@gmail.com>
2014-08-04 15:01:54 -0700

    add pages, snake

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,3 @@
+class PagesController < ApplicationController
+  def snake; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  
+  get '/snake' => 'pages#snake'
 end
  
