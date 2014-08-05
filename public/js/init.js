f39d63582c036d5f471e04187fdde3839bf6eb71
Gabe Radovsky <gabriel.radovsky@gmail.com>
2014-08-05 00:30:43 -0700

    more content/links to projects

--- public/js/init.js
+++ public/js/init.js
@@ -131,17 +131,17 @@
 			}
 			
 		// Poptrox.
-			$('.gallery').poptrox({
-				useBodyOverflow: false,
-				usePopupEasyClose: false,
-				overlayColor: '#0a1919',
-				overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
-				usePopupDefaultStyling: false,
-				usePopupCaption: true,
-				popupLoaderText: '',
-				windowMargin: 10,
-				usePopupNav: true
-			});
+            // $('.gallery').poptrox({
+            //                 useBodyOverflow: false,
+            //                 usePopupEasyClose: false,
+            //                 overlayColor: '#0a1919',
+            //                 overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
+            //                 usePopupDefaultStyling: false,
+            //                 usePopupCaption: true,
+            //                 popupLoaderText: '',
+            //                 windowMargin: 10,
+            //                 usePopupNav: true
+            //             });
 			
 	});
 
