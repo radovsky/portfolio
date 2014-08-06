7168d317846e30718d8f3e94248e883f5ca64d83
Gabe Radovsky <gabriel.radovsky@gmail.com>
2014-08-06 00:25:08 -0700

    let's try one more thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,9 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
   
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  
+  # VERY TROUBLESOME
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
